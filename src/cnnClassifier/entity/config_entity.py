f7de6256c81e71cb4fa10648d866c8c313abbf97
unknown <rohitahirwar9718@gmail.com>
2025-01-10 00:16:35 +0530

    Resolve tracking issues

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,12 +19,14 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+    
 
 @dataclass(frozen=True)
 class PrepareCallbacksConfig:
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: Path
+
 
 @dataclass(frozen=True)
 class TrainingConfig:
@@ -36,3 +38,12 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
